21b37581d0349557a6fbeeab02db6e2b3cd476fb
Chinna Reddy Nalimela <nchinnareddy@gmail.com>
2011-07-10 13:33:21 +0530

    some

--- app/models/servicelisting.rb
+++ app/models/servicelisting.rb
@@ -21,10 +21,18 @@ class Servicelisting < ActiveRecord::Base
                   end
                  item.status = "expired"
                  item.save
+                 highestbid = nil
                  item.bids.each do |bid|
+                   if highestbid == nil
+                     highestbid = bid
+                   end
+                   if bid.bidprice > highestbid.bidprice
+                      highestbid = bid
+                   end
                  user = User.find_by_id(bid.user_id)
                  Notifier.bid_expired_email(user).deliver  
-               end                           
+             end
+             capturemoney(highestbid)
             end
           end
          end
@@ -32,6 +40,38 @@ class Servicelisting < ActiveRecord::Base
   
   end
 
+def capturemoney(highestbid)
+  user = User.find_by_id(highestbid.user_id)
+  ccard = user.credit_card
+  @order = Order.create(:amount => highestbid.bidprice,                        
+                        :first_name => ccard.first_name,
+                        :last_name => ccard.last_name,
+                        :card_type => ccard.card_type,
+                        :card_number => ccard.card_number,
+                        :card_verification => ccard.card_verification,
+                        :card_expires_on => ccard.card_expires_on,
+                        :address => ccard.address,
+                        :city => ccard.city,
+                        :state_name => ccard.state_name,
+                        :country => ccard.country,
+                        :zip => ccard.zip
+                       )
+  
+end
+
+def standard_purchase_options
+    {
+        :ip => request.remote_ip,
+        :billing_address => {
+        :name     => @order.first_name + @order.last_name,
+        :address1 => @order.address,
+        :city     => @order.city,
+        :state    => @order.state_name,
+        :country  => @order.country,
+        :zip      => @order.zip
+       }
+     }
+end
 
 
 end
