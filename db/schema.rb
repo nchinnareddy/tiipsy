da06606da89aab6c682b3baab9e5f02498d7871b
pravinmishea88 <pravinmishra88@gmail.com>
2011-07-29 20:52:49 +0530

    testing from my lapi

--- db/schema.rb
+++ db/schema.rb
@@ -124,7 +124,9 @@ ActiveRecord::Schema.define(:version => 20110728113615) do
 
   create_table "orders", :force => true do |t|
     t.integer  "user_id"
+    t.integer  "servicelisting_id"
     t.string   "description"
+    t.string   "ip_address"
     t.integer  "amount"
     t.string   "state"
     t.string   "express_token"
@@ -154,6 +156,7 @@ ActiveRecord::Schema.define(:version => 20110728113615) do
     t.integer  "no_of_guests"
     t.float    "highestbid",         :default => 0.0
     t.string   "status",             :default => "inactive"
+    t.integer  "winner_id",          :default => 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "photo_file_name"
@@ -161,13 +164,6 @@ ActiveRecord::Schema.define(:version => 20110728113615) do
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
     t.string   "city"
-  end
-
-  create_table "transactions", :force => true do |t|
-    t.float    "amount"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "twitter_followers", :force => true do |t|
