368384aea8b7cad08279b43bbfb32bd7edb12b17
pravinmishra88 <pravinmishra88@gmail.com>
2011-08-01 15:55:32 +0530

    ip address default servicelisting

--- app/models/bar_bussiness.rb
+++ app/models/bar_bussiness.rb
@@ -1,2 +1,6 @@
 class BarBussiness < ActiveRecord::Base
+  validates_presence_of :name, :message => "Please enter bar Name"
+  validates_presence_of :person_of_contact, :message => "Please enter Person of Contact"
+  validates_presence_of :email, :message => "Email is mandatory field, it can't be blanck"
+  validates_presence_of :address, :message => "Please enter address of Bar"  
 end
--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Socialstock
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    config.autoload_paths << "#{config.root}/lib"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]    
