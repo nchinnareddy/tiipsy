82aae5aa942c7fd42647ab0b8172eceb1982581e
pravinmishea88 <pravinmishra88@gmail.com>
2011-08-02 20:28:02 +0530

    asome

--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,9 @@ ActiveRecord::Schema.define(:version => 20110801134038) do
 
   create_table "orders", :force => true do |t|
     t.integer  "user_id"
+    t.integer  "servicelisting_id"
     t.string   "description"
+    t.string   "ip_address"
     t.integer  "amount"
     t.string   "state"
     t.string   "express_token"
@@ -173,6 +175,7 @@ ActiveRecord::Schema.define(:version => 20110801134038) do
     t.integer  "no_of_guests"
     t.float    "highestbid",         :default => 0.0
     t.string   "status",             :default => "inactive"
+    t.integer  "winner_id",          :default => 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "photo_file_name"
@@ -180,13 +183,6 @@ ActiveRecord::Schema.define(:version => 20110801134038) do
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
     t.string   "city"
-  end
-
-  create_table "transactions", :force => true do |t|
-    t.float    "amount"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "twitter_followers", :force => true do |t|
