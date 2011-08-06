343cff65ca722a049c69aad6df5ed8df00c0e59f
pravinmishra88 <pravinmishra88@gmail.com>
2011-08-06 18:40:03 +0530

    email ajax validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110805062504) do
+ActiveRecord::Schema.define(:version => 20110805121725) do
 
   create_table "admins", :force => true do |t|
     t.float    "bidding_fee"
@@ -240,6 +240,7 @@ ActiveRecord::Schema.define(:version => 20110805062504) do
     t.integer  "zip"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "barowner",           :default => false
   end
 
 end
