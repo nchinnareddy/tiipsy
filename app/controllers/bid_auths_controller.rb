3de4383c8cc16d256016ab0fc6f9a28de4248ae0
Chinna Reddy Nalimela <nchinnareddy@gmail.com>
2011-08-12 16:31:10 +0530

    some

--- app/controllers/bid_auths_controller.rb
+++ app/controllers/bid_auths_controller.rb
@@ -1,13 +1,4 @@
 class BidAuthsController < ApplicationController
   
-def authorize
-    @authamount = Admin.first
-    if @authamount != nil
-    @amount = @authamount.bidding_fee
-  else
-    @amount = 100
-   end
-  end
-
 
 end
