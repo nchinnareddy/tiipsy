d05d7ac3929c8c8b59f0c8ac376e26c037795e88
pravinmishra88 <pravinmishra88@gmail.com>
2011-08-30 13:26:45 +0530

    UI change

--- app/models/bar_bussiness.rb
+++ app/models/bar_bussiness.rb
@@ -8,10 +8,13 @@ class BarBussiness < ActiveRecord::Base
          "<h4>#{name}</h4>" << "<h4>#{address}</h4>"
        end
 
-  has_attached_file :photo
+  has_attached_file :photo 
+  #validates_attachment_size :photo, :less_than => 2.megabytes , :message => "must be less than 2 megabytes"
   
   validates_presence_of :name, :message => "Please enter bar Name"
   validates_presence_of :person_of_contact, :message => "Please enter Person of Contact"
+  validates_numericality_of :phone , :message => "Phone should be number"
+  #validates_inclusion_of :website, :in => %w(http://), :message => "extension http:// is not included in the list"
   validates_presence_of :email, :message => "Email is mandatory field, it can't be blanck"
   validates_presence_of :address, :message => "Please enter address of Bar" 
   validates_uniqueness_of :name , :message => "This bar name is already registerd"
