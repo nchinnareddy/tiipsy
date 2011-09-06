cc278c099f0fadea006da415aa8c786b4ac23c29
pravinmishra88 <pravinmishra88@gmail.com>
2011-09-06 18:25:24 +0530

    paypal testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,54 +52,39 @@ Socialstock::Application.configure do
   
 
   
-#  config.after_initialize do
-#     ActiveMerchant::Billing::Base.mode = :test 
-#  end
+  config.after_initialize do
+     ActiveMerchant::Billing::Base.mode = :test 
+  end
 
-#  config.to_prepare do
-#  OrderTransaction.gateway =  ActiveMerchant::Billing::PaypalGateway.new( 
-#    :login => 'nchinn_1309774589_biz_api1.gmail.com',
-#    :password => '1309774643',
-#    :signature => 'A4ZmaZtYP56ugAWbYNAcjqstyYtgAoBxWlcZnfA.iqDK4CgHeuwWpm12' )
+  config.to_prepare do
+  OrderTransaction.gateway =  ActiveMerchant::Billing::PaypalGateway.new( 
+    :login => 'nchinn_1309774589_biz_api1.gmail.com',
+    :password => '1309774643',
+    :signature => 'A4ZmaZtYP56ugAWbYNAcjqstyYtgAoBxWlcZnfA.iqDK4CgHeuwWpm12' )
   
-#  OrderTransaction.xpressgateway =  ActiveMerchant::Billing::PaypalExpressGateway.new( 
-#    :login => 'nchinn_1307094132_biz_api1.gmail.com',
-#    :password => '1307094143',
-#    :signature => 'A3tSrUJhWQkOjSs.LnbMRFOlOFN3AdRRcOCmTIWXkXK8x5Pn4e93CiVB' )
-#    ::EXPRESS_GATEWAY = OrderTransaction.xpressgateway
-#  end
+  OrderTransaction.xpressgateway =  ActiveMerchant::Billing::PaypalExpressGateway.new( 
+    :login => 'nchinn_1307094132_biz_api1.gmail.com',
+    :password => '1307094143',
+    :signature => 'A3tSrUJhWQkOjSs.LnbMRFOlOFN3AdRRcOCmTIWXkXK8x5Pn4e93CiVB' )
+    ::EXPRESS_GATEWAY = OrderTransaction.xpressgateway
+  end
 
 #  config.after_initialize do
 #     ActiveMerchant::Billing::Base.mode = :production 
 #  end 
-if ENV['RAILS_ENV'] != 'production' 
- config.to_prepare do
- OrderTransaction.gateway =  ActiveMerchant::Billing::PaypalGateway.new( 
-    :login => 'gashaw02_api1.yahoo.com',
-    :password => 'H5F6DNHE63S54WNZ',
-    :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za',
-    :test => 'true')
+ #config.to_prepare do
+ # OrderTransaction.gateway =  ActiveMerchant::Billing::PaypalGateway.new( 
+ #   :login => 'gashaw02_api1.yahoo.com',
+ #   :password => 'H5F6DNHE63S54WNZ',
+ #   :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za' )
   
-  OrderTransaction.xpressgateway =  ActiveMerchant::Billing::PaypalExpressGateway.new( 
-    :login => 'gashaw02_api1.yahoo.com',
-    :password => 'H5F6DNHE63S54WNZ',
-    :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za' )
-    ::EXPRESS_GATEWAY = OrderTransaction.xpressgateway
-  end
-else 
-  config.to_prepare do
-  OrderTransaction.gateway =  ActiveMerchant::Billing::PaypalGateway.new( 
-    :login => 'gashaw02_api1.yahoo.com',
-    :password => 'H5F6DNHE63S54WNZ',
-    :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za')
-  
-  OrderTransaction.xpressgateway =  ActiveMerchant::Billing::PaypalExpressGateway.new( 
-    :login => 'gashaw02_api1.yahoo.com',
-    :password => 'H5F6DNHE63S54WNZ',
-    :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za' )
-    ::EXPRESS_GATEWAY = OrderTransaction.xpressgateway
-  end
-end
+ # OrderTransaction.xpressgateway =  ActiveMerchant::Billing::PaypalExpressGateway.new( 
+ #   :login => 'gashaw02_api1.yahoo.com',
+ #   :password => 'H5F6DNHE63S54WNZ',
+ #   :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31A3iVwYD55DBbNgP13gSWebVQj.za' )
+ #   ::EXPRESS_GATEWAY = OrderTransaction.xpressgateway
+ # end
+
 
 end
 
