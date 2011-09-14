bed52d59d65178828c0b3cc619d932e56f92042a
pravinmishra88 <pravinmishra88@gmail.com>
2011-09-14 11:24:16 +0530

    Revert "Revert "Revert "service"""
    This reverts commit 350450222f1774dd62cc71e084b5b7daa25fc713.

--- db/migrate/20110913083258_add_to_servicelisting_minbid.rb
+++
@@ -1,9 +0,0 @@
-class AddToServicelistingMinbid < ActiveRecord::Migration
-  def self.up
-    add_column :servicelistings, :min_bid, :string
-  end
-
-  def self.down
-    remove_column :servicelistings, :min_bid
-  end
-end
