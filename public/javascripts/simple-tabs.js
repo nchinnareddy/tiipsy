ba88e49de784f04c96ef6b3fe914dd29dc4a79a6
Prasanna <prasofty@gmail.com>
2012-01-05 10:44:45 +0530

    fixed activetab click

--- public/javascripts/simple-tabs.js
+++ public/javascripts/simple-tabs.js
@@ -11,12 +11,18 @@ $(function(){
 		$(".tab_content:first").show(); //Show first tab content		
 	}	
 	
-	$("ul.simple-tabs li").click(function() {		
+	$("ul.simple-tabs li").click(function() {
+        var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+
+        if($(activeTab).is(':visible')) {
+            return false;
+        }
+
 		$("ul.simple-tabs li").removeClass("active"); //Remove any "active" class
 		$(this).addClass("active"); //Add "active" class to selected tab
 		$(".tab_content").hide(); //Hide all tab content
 		
-		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+
 		$(activeTab).fadeIn(); //Fade in the active ID content
 		return false;		
 	});
