21f396ab29f3b3390c0ff364eb81cf42f78b6463
Prasanna <prasofty@gmail.com>
2012-01-07 16:17:47 +0530

    modifed gmail contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,56 @@
 class ContactsController < ApplicationController
+  before_filter :require_user
+
+  def index
+    @contacts = current_user.contacts
+  end
+
+  def new
+    redirect_to Google::Authorization.build_auth_url("#{root_url}contacts/get_contacts")
+  end
+
+  def get_contacts
+    token = Google::Authorization.exchange_singular_use_for_session_token(params[:token])
+    unless token == false
+      @contacts = Google::Contact.all(token)
+      @contacts.each do |contact|
+        Contact.create({
+            :user_id => current_user.id,
+            :source => 'gmail',
+            :username => contact.name,
+            :email => contact.email
+        })
+      end
+    else
+      flash[:error] = "Something went wrong while authorizing with google."
+    end
+
+    redirect_to contacts_path
+  end
+
+  #TODO old code
+=begin
   def new
     #raise params.to_yaml
-     $no_of_guests = params[:no_of_guests]
-     $user_name = params[:user_name]
-     $product = params[:product]
+    $no_of_guests = params[:no_of_guests]
+    $user_name = params[:user_name]
+    $product = params[:product]
     if ENV['RAILS_ENV'] == "development"
       redirect_to Google::Authorization.build_auth_url("http://localhost:3000/contacts/authorize")
     else
       redirect_to Google::Authorization.build_auth_url("http://socialcheers.com/contacts/authorize")
     end
   end
-  
+
   def authorize
     token = Google::Authorization.exchange_singular_use_for_session_token(params[:token])
-    raise token.inspect 
 
     unless token == false
       if ENV['RAILS_ENV'] == "development"
         redirect_to "http://localhost:3000/contacts?token=#{token}"
       else
-        redirect_to "http://socialcheers.com/contacts?token=#{token}"  
-      end 
+        redirect_to "http://socialcheers.com/contacts?token=#{token}"
+      end
     else
       flash[:error] = "Something went wrong while authorizing with google."
     end
@@ -29,7 +59,7 @@ class ContactsController < ApplicationController
   def show
     @contacts = Google::Contact.all(params[:token])
   end
-  
+
   def mail
     #raise params.to_yaml
     #raise @contacts = Contact.new(params[:contact]).inspect
@@ -68,5 +98,6 @@ class ContactsController < ApplicationController
     #raise params.to_yaml
     raise @guest = GuestList.find(params[:id]).inspect
   end
+=end
     
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
+
+  validates_uniqueness_of :email, :scope => [:user_id]
 end
